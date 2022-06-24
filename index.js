4dd73dde7390e8eb97c2130fb4af690c10dadcb9
Ruzell11 <johnruzell123@gmail.com>
2022-06-24 18:40:02 +0800

    checkbox value

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var email = document.querySelector('.email');
 var password = document.querySelector('.password');
-var checkbox = document.querySelector('.checkbox');
+var checkbox = document.getElementById('checkbox');
 var btn = document.querySelector('.btn');
 var form = document.querySelector('.form');
 var container = document.querySelector('.container')
@@ -11,31 +11,32 @@ var container = document.querySelector('.container')
     var emailValue = 'user123@gmail.com' 
     var passwordValue = 'user123'
 
-    if(email.value && password.value){
-        btn.removeAttribute("disabled");
-     btn.addEventListener('click' , () => {
-        if(email.value === emailValue && password.value === passwordValue){
-            container.innerHTML = "Welcome Ruz!!"
-            email.classList.remove('redBorder');
+    if(email.value && password.value  && checkbox.addEventListener('click' , () => {
+        if(checkbox.checked){
+            btn.removeAttribute("disabled")
+            btn.addEventListener('click' , () => {
+                if(email.value === emailValue && password.value === passwordValue){
+                    window.location.href = "https://ruzell11.github.io/final-portfolio/"
+                    container.innerHTML = "Welcome Ruz!!"
+                    email.classList.remove('redBorder');
+                }else{
+                    container.innerHTML = "Welcome User!"
+                }   
+            })
         }else{
-            container.innerHTML = "Welcome User!"
-        }   
-     }) 
-    }else{
-        btn.disabled = true;
-       
-    }
+            btn.disabled = true;
+        }
+    }))
+    {
 
-
-};
+    
+     
+           
+        }
+     }
 form.addEventListener('submit' , (event) => {
     event.preventDefault()
     
-
-    // window.location.href =  "https://ruzell11.github.io/final-portfolio/"
-   
-    
 });
-
 
 
