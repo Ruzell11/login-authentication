b7c51a0665b5570030cf036d0b39ebc081d10c77
Ruzell11 <johnruzell123@gmail.com>
2022-06-24 19:21:36 +0800

    login security

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ var password = document.querySelector('.password');
 var checkbox = document.getElementById('checkbox');
 var btn = document.querySelector('.btn');
 var form = document.querySelector('.form');
-var container = document.querySelector('.container')
+var container = document.querySelector('.container');
+var errors = document.querySelector('.errors');
+var errorContainerEmail = document.querySelector('.errorcontainer');
+var errorContainerPass = document.querySelector('.errorcontainerPass');
 
 
  const isEmpty = () => {
 
     var emailValue = 'user123@gmail.com' 
     var passwordValue = 'user123'
+    var passwordGuest = "guest123"
 
     if(email.value && password.value  && checkbox.addEventListener('click' , () => {
         if(checkbox.checked){
@@ -20,8 +24,20 @@ var container = document.querySelector('.container')
                     container.innerHTML = "Welcome Ruz!!"
                     email.classList.remove('redBorder');
                 }else{
-                    container.innerHTML = "Welcome User!"
-                }   
+                    if(password.value != passwordValue && email.value != emailValue){
+                        errors.innerHTML = "Invalid Email and Password"
+                    } else if(email.value != emailValue){
+                        errorContainerEmail.innerHTML = "Invalid Email"
+                        errors.innerHTML = ""
+                    }else if(password.value != passwordValue){
+                        errorContainerPass.innerHTML = "Invalid Pass"
+                        errors.innerHTML = ""
+                    }
+                }if(email.value && password.value === passwordGuest){
+                   errors.innerHTML = ""
+                    container.innerHTML = "Welcome Guest"
+                    window.location.href = "https://www.youtube.com/"
+                }
             })
         }else{
             btn.disabled = true;
